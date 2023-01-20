0baea4bd592685701170cf6dcf3412b6829a2d90
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-01-20 16:58:18 -0300

    Update exibir_senha.js

--- api/assets/js/exibir_senha.js
+++ api/assets/js/exibir_senha.js
@@ -9,3 +9,12 @@ function mostrarOcultarSenha() {
     senha.type = "password";
   }
 }
+function mostrarOcultarSenha2() {
+  var senha = document.getElementById("senha2");
+
+  if (senha.type == "password") {
+    senha.type = "text";
+  } else {
+    senha.type = "password";
+  }
+}
